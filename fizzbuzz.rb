ad753fd3c6af60f21a022697cf207a13de57dfa4
Elliott King <8884571+elliott-king@users.noreply.github.com>
2020-02-01 19:55:15 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,13 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(m)
+  s = ""
+  if m % 3:
+    s += "Fizz"
+  if m % 5:
+    s += "Buzz"
+  if s.size > 0:
+    return s
+  return m
+end
